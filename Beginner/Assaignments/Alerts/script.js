a86b67de0944099f1b474cdfa92b3908c66631c0
Abid Hussain <abidhussainkhaskheli786a@gmail.com>
2025-05-10 00:19:17 +0500

    Add Assignments

---
+++ Beginner/Assaignments/Alerts/script.js
@@ -0,0 +1,12 @@
+// 1. Greet website visitor
+alert("Welcome to my website!");
+
+// 2. Display message on web page
+alert("Error! Please enter a valid password.");
+
+// 3. Display message with line break
+alert("Welcome to JS Land... \nHappy Coding!");
+
+// 4. Display messages in sequence
+alert("Welcome to JS Land...");
+alert("Happy Coding! \n Prevent this page from creating additional dialogs");
---
+++ Beginner/Assaignments/Variable_Strings_Numbers/script.js
@@ -0,0 +1,82 @@
+// 1. Declare variable username
+var username;
+
+// 2. Declare myName
+var myName = "Abid Hussain";
+
+// 3. Declare and show message in alert box
+var message;
+message = "Hello World";
+alert(message);
+
+// 4. Student bio data
+var studentName, studentAge, studentInstitute, studentCourse;
+
+ studentName = "Ali Khan";
+ studentAge = 18;
+ studentInstitute = "SMIT";
+ studentCourse = "Web Development";
+
+ console.log("#4: ");
+
+console.log("Name: " + studentName);
+console.log("Age: " + studentAge);
+console.log("Institute: " + studentInstitute);
+console.log("Course: " + studentCourse);
+
+// 5. Age
+var age = 14;
+
+console.log("#5: ");
+
+console.log("I am " + age + " years old");
+
+// 6. Visitor count
+var visitCount = 5;
+
+document.write("#6: ");
+
+document.write("You have visited this site " + visitCount + " times<br><br>");
+
+// 7. Birth year
+var birthYear = 2005;
+
+document.write("#7: ");
+
+document.write("My birth year is " + birthYear + "<br>");
+document.write("Data type of my declared variable is " + typeof birthYear + "<br>");
+
+// 8. One variable console pattern
+var pizza = "pizza";
+
+console.log("#8: ");
+
+console.log(pizza);
+console.log("pizz");
+console.log("piz");
+console.log("pi");
+console.log("p");
+
+// 9. Fortune Teller
+var children, partner, geoLocation, job;
+
+children = 3;
+partner = "Sana";
+geoLocation = "Canada";
+job = "Software Engineer";
+
+console.log("#9: ");
+
+console.log("You will be a " + job + " in " + geoLocation + ", and married to " + partner + " with " + children + " kids.");
+
+// 10. Email
+var email = "abc@gmail.com";
+
+console.log("#10: ");
+
+alert("My email address is " + email);
+console.log("My email address is " + email);
+
+// 11. Book
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book '" + book + "'");
